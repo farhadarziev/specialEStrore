74a38a3f014fafb2bc763df3d078b1f4fe8cbfcb
Farhad Arziev <farhadarziev8@gmail.com>
2026-03-29 20:51:26 +0600

    implement product catalog API

---
+++ src/main/java/com/example/estore/dto/RegistrationRequest.java
@@ -0,0 +1,52 @@
+package com.example.estore.dto;
+
+
+public class RegistrationRequest {
+    private String login;
+    private String password;
+    private String name;
+    private String surname ;
+    private String phoneNum;
+
+    public RegistrationRequest(){}
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+}
